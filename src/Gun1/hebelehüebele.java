36e717881d9e23f9b8dece4baf107a9ae4263902
Alp Eren TOKSOY <alperentoksoy95@gmail.com>
2022-10-12 09:49:27 +0300

    yine selam verdik dünyaya489

--- src/Gun1/hebelehüebele.java
+++ src/Gun1/hebelehüebele.java
@@ -3,5 +3,7 @@ package Gun1;
 public class hebelehüebele {
     public static void main(String[] args) {
         System.out.println("selam yine dünya mk");
+        System.out.println("selam yine dünya mk");
+        System.out.println("selam yine dünya mk");
     }
 }
